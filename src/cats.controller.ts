248c274681fff181972cc0c2cb88ed68a54ca6b5
Daniel Yordanov <daniyord@gmail.com>
2022-05-10 00:18:02 +0300

    -

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Query, HttpCode } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @HttpCode(204)
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(): string {
+  findAll(@Req() request: Request, @Query() key?: string): string {
+    // console.log(request);
+    console.log(key);
     return 'This action returns all cats';
   }
+
+  //   @Get()
+  //   findAll(): any {
+  //     return {
+  //       demo: 1,
+  //       demo1: 'tt',
+  //     };
+  //   }
 }
