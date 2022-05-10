dfa24ec886784d2f3a03f87009b407c10051591c
Daniel Yordanov <daniel.yordanov@a1.bg>
2022-05-10 10:47:33 +0300

    -

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,26 +1,41 @@
-import { Controller, Get, Post, Req, Query, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
 import { Request } from 'express';
+
+import { GetCatsDto, CreateCatDto, UpdateCatDto } from './dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  @HttpCode(204)
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto): string {
     return 'This action adds a new cat';
   }
 
   @Get()
-  findAll(@Req() request: Request, @Query() key?: string): string {
-    // console.log(request);
-    console.log(key);
-    return 'This action returns all cats';
+  findAll(@Query() query: GetCatsDto): string {
+    return `This action returns all cats (limit: ${query.limit} items)`;
   }
 
-  //   @Get()
-  //   findAll(): any {
-  //     return {
-  //       demo: 1,
-  //       demo1: 'tt',
-  //     };
-  //   }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
 }
